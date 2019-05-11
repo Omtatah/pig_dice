3e03c32bffc13c9055d757e00dbd100436cd8228
Omtatah <murtallahomtatah@gmail.com>
2019-05-11 22:51:00 +0300

    add the user logic functionality to allow switching of slides on the game.html file

---
+++ js/game.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('#l_portal').click(function(){
+        $('.col-md-4').hide();
+        $('#col-md-8-r').show();
+        $('#l_portal').hide();
+        $('.g_portal_1').show();
+        $('#col-md-8-h').hide();
+        $('.g_portal_2').hide();
+        $('#r_portal').show();
+    })
+    $('#r_portal').click(function(){
+        $('.col-md-4').hide();
+        $('#col-md-8-h').show();
+        $('#col-md-8-r').hide();
+        $('#r_portal').hide();
+        $('.g_portal_2').show();
+           $('.g_portal_1').hide();
+        $('#l_portal').show();
+   })
+    $('.g_portal_1').click(function(){
+        $('.col-md-4').show();
+        $('#col-md-8-r').hide();
+        $('#l_portal').show();
+        $('.g_portal_1').hide();
+    })
+    
+    $('.g_portal_2').click(function(){
+        $('.col-md-4').show();
+        $('#col-md-8-h').hide();
+        $('#r_portal').show();
+        $('.g_portal_2').hide();
+    })
+})
