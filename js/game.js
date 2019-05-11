dfd2625e5798d894173df6718363d17dfa87c321
Omtatah <murtallahomtatah@gmail.com>
2019-05-11 22:54:04 +0300

    properly indent my code on all pages; add two images of possible use to the images folder of the project directory

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('#l_portal').click(function(){
+$(document).ready(function () {
+    $('#l_portal').click(function () {
         $('.col-md-4').hide();
         $('#col-md-8-r').show();
         $('#l_portal').hide();
@@ -8,23 +8,22 @@ $(document).ready(function(){
         $('.g_portal_2').hide();
         $('#r_portal').show();
     })
-    $('#r_portal').click(function(){
+    $('#r_portal').click(function () {
         $('.col-md-4').hide();
         $('#col-md-8-h').show();
         $('#col-md-8-r').hide();
         $('#r_portal').hide();
         $('.g_portal_2').show();
-           $('.g_portal_1').hide();
+        $('.g_portal_1').hide();
         $('#l_portal').show();
-   })
-    $('.g_portal_1').click(function(){
+    })
+    $('.g_portal_1').click(function () {
         $('.col-md-4').show();
         $('#col-md-8-r').hide();
         $('#l_portal').show();
         $('.g_portal_1').hide();
     })
-    
-    $('.g_portal_2').click(function(){
+    $('.g_portal_2').click(function () {
         $('.col-md-4').show();
         $('#col-md-8-h').hide();
         $('#r_portal').show();
